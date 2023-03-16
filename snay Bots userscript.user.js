9a2935d1a672d136ef011f9bc7526ea76bacee19
ShazamHax <116787686+ShazamHax@users.noreply.github.com>
2023-03-16 15:43:18 +0800

    Update snay Bots userscript.user.js

--- snay Bots userscript.user.js
+++ snay Bots userscript.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Shazam's Snay.io script
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  A script that updates automatically, to bring you cool features for Snay.io.
 // @author       Shazam
-// @match        https://www.snay.io
+// @match        https://www.snay.io/*
 // @icon         https://yt3.googleusercontent.com/TNVorEJ9iTsESmUbcZXizwaZgy5jB-Ihx3z9qxfuuatrFRDhJHotz5x_X7mGIu38VBsr5bvlkg=s176-c-k-c0x00ffffff-no-rj
 // @grant        none
 // ==/UserScript==
